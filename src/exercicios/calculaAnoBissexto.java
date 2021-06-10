b6dde366cfae5e4fc7e5ff454cf51bb09fabac01
Fábio Souza <74217251+Fabio-SS@users.noreply.github.com>
2021-06-10 15:13:05 -0300

    Update
    Atualizado código do calculo

--- src/exercicios/calculaAnoBissexto.java
+++ src/exercicios/calculaAnoBissexto.java
@@ -4,28 +4,21 @@ import java.util.Scanner;
 
 public class calculaAnoBissexto {
 		
-		//Calcular se um ano é bissexto ou não.
+		//Calculo se um ano é bissexto ou não.
 	
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.printf("Digite o ano: ");
-		double ano = entrada.nextInt();
+		int ano = entrada.nextInt();
 		
-		if(ano % 4 == 0 && ano % 100 != 0) {
-			System.out.println("O " + ano + " é bissexto");
-			if(ano % 100 == 0 && ano % 400 != 0) {
-				System.out.println("O" + ano + "Não é bissexto");
-				if(ano % 100 == 0 && ano % 400 == 0) {
-					System.out.println("O" + ano + "é bissexto");
-				}
-			}
-		}else {
-			System.out.println("Não é bissexto");
-		}
+		if((ano % 4 == 0 && ano % 100 != 0) || (ano % 100 == 0 && ano % 400 == 0)) 
+			System.out.println("Ano Bissexto");
+		else
+			System.out.println("Não é ano bissexto");
+		
 		entrada.close();
 		
 	};
 }
-//Analisar código pois alguns anos bissextos estão sendo informados como não bissexto.
